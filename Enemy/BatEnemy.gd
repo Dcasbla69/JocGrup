37ba764a19938063706a797d4dd09c62d3490e53
ArnauCarrasco02 <127753878+Dcasbla69@users.noreply.github.com>
2023-04-24 13:57:13 +0200

    ordenar directoris

--- Enemy/BatEnemy.gd
+++ Enemy/BatEnemy.gd
@@ -9,10 +9,6 @@ var movespeed = 50
 onready var player = get_parent().get_node("Player")
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
