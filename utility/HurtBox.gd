7aa64186b1f7e33d3abd0610f5095a95d5ff3ae7
RegNugg <73636109+Dipo3@users.noreply.github.com>
2023-04-25 09:45:16 +0200

    a

--- utility/HurtBox.gd
+++ utility/HurtBox.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
 
-
 enum DROPOFF { Cooldown,HitOnce,DissableHitBox }
 export(DROPOFF) var HurtBoxType = DROPOFF.empty()
+
+onready var collision = $CollisionShape2D
+onready var dissabletimer = $DisableTimer
+
+
+signal hurt(damage)
